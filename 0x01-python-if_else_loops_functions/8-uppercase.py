d514268e2af39a3b1185585a8190ed9f31d55143
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-31 17:47:08 -0700

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+def islower(c):
+    if (ord(c) >= ord('a') and ord(c) <= ord('z')):
+        return True
+    else:
+        return False
+
 def uppercase(str):
     for char in str:
         print("{:c}"
