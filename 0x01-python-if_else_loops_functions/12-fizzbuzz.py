b5ccb14b53f8ef09aad4236f5a4412b54af62a1f
Moniaar <www.omniaahmed1562001@gmail.com>
2023-10-31 18:43:50 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for x in range(1, 101):
         if (x % 3 == 0):
             print("Fizz", end=" ")
-        elif (x % 3 == 0):
+        elif (x % 5 == 0):
             print("Buzz", end=" ")
         elif (x % 15 == 0):
             print("FizzBuzz", end=" ")
