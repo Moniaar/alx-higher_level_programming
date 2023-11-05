531745bafe40eabfe8f60ca246089785fd5421d3
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-05 14:38:03 -0800

    13. Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * palin - function to know if it's palindrome
+ * @head: the head of the list
+ * @end: end of the list
+ * Return: always 1 since it's recursive
+ */
+
+int palin(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (palin(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+}
+
+/**
+ * is_palindrome - function to check if it's recursive function
+ * or not
+ * @head: the head of the list
+ * Return: - if it's not palindrome, 1 if it is
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (palin(head, *head));
+}
