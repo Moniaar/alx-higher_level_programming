af97dfab6efe51486111fe043883aae80cb0158b
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-20 11:04:51 -0800

    Pleasse work

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j, k = 0, 0
+    while j < x:
+        try:
+            print("{:d}" .format(my_list=[]), end ='')
+            k += 1
+        except (ValueError, TypeError):
+            pass
+        j += 1
+    print()
+    return k
