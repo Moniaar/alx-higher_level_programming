801aaf010ae12dd9048199d60cfa697b30d31d6a
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-20 11:12:34 -0800

    3. Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    Divres = 0
+
+    try:
+        Divres = a / b
+    except ZeroDivisionError:
+        Divres = None
+    finally:
+        print('Inside result: ()'.format(Divres))
+        return Divres
