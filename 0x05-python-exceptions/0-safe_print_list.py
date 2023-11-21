f268bcfb2faa775f658768294a8d834eab342d90
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-20 18:11:20 -0800

    Fixed index error inside the print formatting

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     k = 0
     try:
         while k != x:
-            print(my_list, end='')
+            print(my_list[k], end='')
             k += 1
     except IndexError:
-        pass
+        None
     print()
     return k
