0e20669a4a425a0105fa8e5c5fd71efca69f02b6
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-11-29 06:31:41 +0000

    Omitted some lines of code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -13,20 +13,3 @@ class LockedClass:
     __setattr__(self, name, value): Overrides the default attribute setting
         behavior to allow only the attribute 'first_name' to be set."""
     __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        """Overrides the default attribute setting behavior to allow only
-        the attribute 'first_name' to be set.
-
-        Args:
-        name (str): The name of the attribute being set.
-        value: The value to be assigned to the attribute.
-
-        Raises:
-        AttributeError: If an attempt is made to set an attribute other
-            than 'first_name'.
-        """
-
-        if name != 'first_name':
-            raise AttributeError("Can't create new except 'first_name'")
-        super().__setattr__(name, value)
