662dea6c04b40ef5860986f7be183a1b6eea2d74
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2023-11-29 06:30:14 +0000

    I fixed a spelling mistake

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["Bestschool" for m in range(magic_string.count)])
+    return ", ".join(["BestSchool" for m in range(magic_string.count)])
