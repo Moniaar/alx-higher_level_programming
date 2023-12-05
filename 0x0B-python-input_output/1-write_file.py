4c5bf1ca0bf619a70e7082a6e0cadcb6e27e2c67
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-05 10:10:35 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """This is a remarkable module """
 
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """A function to write into a file from a user """
     with open(filename, ,'w', encoding='utf-8') as f:
         return f.write(text)
