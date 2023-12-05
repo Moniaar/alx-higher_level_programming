6a06d82e02433260d0585e692cc5a892646714e0
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-05 10:11:50 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """A function to write into a file from a user """
-    with open(filename, ,'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
