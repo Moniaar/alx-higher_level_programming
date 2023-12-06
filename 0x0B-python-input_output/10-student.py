1c82401ca9f77060263abc7f608c3063df4842b5
Moniaar <www.omniaahmed1562001@gmail.com>
2023-12-05 17:31:45 -0800

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,18 +9,17 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """a function that retrieves a dictionary
-        representation of a Student instance"""
-        return self.__dict__
-    try:
-        for attr in attrs:
-            if type(attr) is not str:
-                return self.__dict__
-    except Exception:
-        return self.__dict__
-    my_dict = dict()
-    for key, value in self.__dict__.items():
-        if key in attrs:
-            my_dict[key] = value
-    return my_dict
+    def to_json(self, attrs=None):
+        """a function  retrieves a dictionary representation
+        of a Student instance"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
