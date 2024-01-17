11d3e20b1495f19c870595784b469ff5b902ba66
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-16 22:34:01 -0800

    #4. First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+--  a script that creates a table called first_table in
+-- the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS mysql.first_table (
+id INT,
+name VARCHAR(256)
+);
