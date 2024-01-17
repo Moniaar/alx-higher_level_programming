bf5ad7d122db7fef419c46ec6eddd53439ab06df
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-16 22:38:15 -0800

    #10. List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- a script that lists all records of the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name
+	FROM hbtn_0c_0.seconed_table ORDER BY score DESC
