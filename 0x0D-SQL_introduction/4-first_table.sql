42532e80c8de03be062097cc959b4c4dda9ac94b
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-17 01:54:31 -0800

    #18 Ommited the server name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 --  a script that creates a table called first_table in
 -- the current database in your MySQL server.
-USE hbtn_0c_0;
-
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS 'first_table' (
+    'id' INT,
+    'name' VARCHAR(256)
 );
