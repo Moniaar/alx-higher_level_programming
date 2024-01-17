0dab4fa082163f7668047b18c05b138e7d55d27d
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-17 02:25:16 -0800

    #23 Removed the single quotes

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- a script that lists all rows of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server.
 -- Datebase name.tableName gives you accsess
-SELECT * FROM 'first_table';
+SELECT * FROM `first_table`;
