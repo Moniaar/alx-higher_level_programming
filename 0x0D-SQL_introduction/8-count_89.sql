73c09e33f05d26cec35de543ac953afc34557bbc
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-17 02:28:16 -0800

    #25 Removed the single quotes

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
 -- a script that displays the number of records with
 -- id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT(id) FROM hbtn_0c_0.first_table
-WHERE id = 89;
+SELECT COUNT (*)
+FROM `first_table`
+WHERE `id` = 89;
