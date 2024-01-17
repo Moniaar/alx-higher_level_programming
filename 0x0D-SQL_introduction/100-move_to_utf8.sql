cf19c016ae4e5fe72dec21496688e022a67380c5
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-17 03:18:24 -0800

    #37 converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@
 -- Convert the database
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET `utf8mb4` COLLATE utf8mb4_unicode_ci;
