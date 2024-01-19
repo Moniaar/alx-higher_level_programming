ee5a4053703ab04f18d873ff680e115652894a4f
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-18 17:49:16 -0800

    #9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id cities.name states.name LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
