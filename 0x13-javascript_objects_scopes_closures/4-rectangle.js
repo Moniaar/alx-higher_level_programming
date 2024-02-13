ac628851e341722efb310918643351c3c6a2fd0b
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-13 17:15:59 +0200

    #16 added two other methods named rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let m = 0; m < this.height; m++) {
+      let Xshape = '';
+      for (let n = 0; n < this.width; n++) Xshape += 'X';
+      console.log(Xshape);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
