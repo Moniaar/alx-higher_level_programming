835a34332a3a84ee2b795535103e662a92faf625
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-13 14:34:43 +0200

    #2 Fixed semistandard issue

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
-if(process.argv[2] !== undefined){
- console.log(process.argv[2]);
-}else{
- console.log('No argument');
+if (process.argv[2] !== undefined) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
