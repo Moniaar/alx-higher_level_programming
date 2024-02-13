3fb1769eb66072a2708caf775ea4c3a43b5904c0
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-13 14:36:55 +0200

    #5 Fixed a semistandard issue

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function factorial(m){
- if (isNaN(m) || m === 0 )return 1;
- return m * factorial(m - 1);
+function factorial (m) {
+  if (isNaN(m) || m === 0) return 1;
+  return m * factorial(m - 1);
 }
 
 console.log(factorial(Number(process.argv[2])));
