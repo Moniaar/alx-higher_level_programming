8d5040233dc10a7945363d99a5e0a62754f05e77
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-13 14:33:49 +0200

    #1 First commit of the daym fixed semistandard issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 const argsLen = process.argv.length - 2;
-if (argsLen === 0){
- console.log('No argument');
-} else if (argsLen === 1){
- console.log('Argument found');
-} else{
- console.log('Arguments found');
+if (argsLen === 0) {
+  console.log('No argument');
+} else if (argsLen === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
