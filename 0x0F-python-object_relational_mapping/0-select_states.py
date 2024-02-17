ea540ce313492247ab58d7883f5c70d6ad3a0995
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-17 16:42:48 +0200

    #7 Trying to add charset=utf8 to see if it will work

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
-import MySQLdb
-db = MySQLdb.connect(host=3306, user=username, passwd=password, db=name)
-ORDER BY `states.id` ASC;
+"""A Script that will lists all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    HOST = "localhost"
+    PORT = 3306
+    USER = argv[1]
+    PASSWORD = argv[2]
+    DATABASE = argv[3]
+    db = MySQLdb.connect(host=HOST,
+                         port=PORT,
+                         user=USER,
+                         passwd=PASSWORD,
+                         db=DATABASE,
+                         charset="utf8")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
