5fc968a24f814bc988effaaf9146eeee2783e71a
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-18 12:16:39 +0200

    #6 selecting a query in a more securitve mannaer

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A Script that will lists all states from the database hbtn_0e_0_usa
+But with the name is mathcing the argument passed! :)
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    HOST = "localhost"
+    PORT = 3306
+    USER = argv[1]
+    PASSWORD = argv[2]
+    DATABASE = argv[3]
+    db = MySQLdb.connect(host=HOST,
+                         port=PORT,
+                         user=USER,
+                         passwd=PASSWORD,
+                         db=DATABASE,
+                         charset="utf8")
+
+    cursor = db.cursor()
+    # The query to search for any argument assoiciated
+    # with the name as a string by avoiding any sql injection attacks
+    query = "SELECT * FROM states WHERE name = %(name)s ORDER BY states.id"
+    # to check the format of the name of the state that is passed using name
+    cursor.exectue(query, {'name':state_name})
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
