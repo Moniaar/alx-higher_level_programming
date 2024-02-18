59171c145e6142b8fe534af73a6681ae6871c0af
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-18 12:33:04 +0200

    #6 selecting a query in a more securitve mannaer

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,19 +11,18 @@ if __name__ == '__main__':
     HOST = "localhost"
     PORT = 3306
     USER = argv[1]
-    PASSWORD = argv[2]
-    DATABASE = argv[3]
+    PASS = argv[2]
+    DB = argv[3]
+    state_name = argv[4]
     db = MySQLdb.connect(host=HOST,
                          port=PORT,
                          user=USER,
-                         passwd=PASSWORD,
-                         db=DATABASE,
+                         passwd=PASS,
+                         db=DB,
                          charset="utf8")
 
     cursor = db.cursor()
-    # The query to search for any argument assoiciated with the name as a string
     query = "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY states.id"
-    # to check the format of the name of the state that is passed using the format method
     cursor.exectue(query.format(state_name))
     rows = cursor.fetchall()
     for row in rows:
