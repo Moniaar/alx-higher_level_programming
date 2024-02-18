5dfc6c03e3cbd9a84ea2c233439fce72bd9ba3b3
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-18 20:33:50 +0200

    #38 same as last task except If the table states is empty, print Nothing followed by a new line

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script to list all State objects from the database hbtn_0e_6_usa."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    # Check if correct number of arguments are provided
+    if len(argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
+              .format(argv[0]))
+        exit(1)
+
+    # Get MySQL credentials from command line arguments
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(mysql_username, mysql_password,
+                                   database_name))
+
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and print the first State object, ordered by states.id
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
