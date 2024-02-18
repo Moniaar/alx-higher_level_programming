9b4e1acf2dd73cdff87ee58ad57c4faf3edf3ddb
Moniaar <WWW.omniaahmed1562001@gmail.com>
2024-02-18 18:49:26 +0200

    #22 Debugging session starts now

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     #The query to search among all states rows
     query = "SELECT * FROM states WHERE name LIKE BINARY
-            '%N' ORDER BY states.id"
+            '%N' ORDER BY id ASC"
 
     cursor.execute(query)
     rows = cursor.fetchall()
