2a321a1aa766120a4f64a7859d25f9cb50c769b1
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-02-29 20:05:17 +0200

    Using email and a special query

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 #a script to sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
