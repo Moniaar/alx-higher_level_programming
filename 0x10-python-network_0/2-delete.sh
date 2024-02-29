9ac53406f2ffc659962c5fb8cad9e1eb93aa9b63
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-02-29 19:56:33 +0200

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use curl to send a GET request and display the body of a 200 status code response
+# Use curl to send a GET request and display the body of # a 200 status code response
 curl -s "$1" -X DELETE
