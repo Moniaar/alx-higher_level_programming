d1481a69383459758413eb4dd76abec501d33c5e
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-02-29 20:04:34 +0200

    #7 one line of code

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-# Sams as task 3 except the header variable X-School-User-Id must be sent with the value 98
-curl -s "$1" -X POST -d
+# Sams as task 3 except the header variable curl -sH "X-School-User-Id: 98" -X GET "$1"
