3b11f593aeee63155fcf396e0352e7a813f369be
moniaar <www.omniaahmed1562001@gmail.com>
2024-02-29 12:46:18 +0200

    #3 This gives me 0 bytes as response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# Check if URL parameter is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
-# Use curl to send a request and display the size of the response body
-RESPONSE_SIZE=$(curl "$URL" -s -w '%{size_upload}')
-echo "$RESPONSE_SIZE"
-
-
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
