f05bd01804762be74f42e21f269b7157fc871404
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-01 08:02:51 +0200

    #4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import requests
+
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    # Send a GET request and display the body of the response
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+
+    # Display the content of the response
+    print(response.text)
+except requests.exceptions.HTTPError as e:
+    print(f"Error code: {e.response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Request error: {e}")
