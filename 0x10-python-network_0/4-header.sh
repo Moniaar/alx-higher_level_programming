45712a54a57638b1e03bdb01200eb84e6d23568b
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-01 07:20:19 +0200

    #9 added a new line

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
+#!/bin/bash
 # using delete to pass the url as a first argument
 curl -s "$1" -X DELETE
 
-=======
-#!/bin/bash
-# Sams as task 3 except the header variable
-curl -sH "X-School-User-Id: 98" -X GET "$1"
->>>>>>> f029d22dabb64dee8c310d4b90e288949e88758c
