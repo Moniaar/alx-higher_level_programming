121fbfa1aa40c3bacaf874d9d75c0efce7264f3a
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-01 08:00:40 +0200

    #4 a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+import urllib.request
+import urllib.error
+import sys
+
+def fetch_url_body(url):
+    try:
+        # Send a request and display the body of the response
+        with urllib.request.urlopen(url) as response:
+            response_body = response.read().decode('utf-8')
+            print(response_body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+if __name__ == "__main__":
+    # Check if URL parameter is provided
+    if len(sys.argv) < 2:
+        print("script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    fetch_url_body(url)
+
