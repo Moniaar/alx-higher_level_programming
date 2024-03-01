f029d22dabb64dee8c310d4b90e288949e88758c
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-03-01 07:06:43 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
-#! /bin/bash
-# Sams as task 3 except the header variable curl -sH "X-School-User-Id: 98" -X GET "$1"
+#!/bin/bash
+# Sams as task 3 except the header variable
+curl -sH "X-School-User-Id: 98" -X GET "$1"
