701529ef10b958f9ebe73327ad5b42aab3028f27
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 06:25:16 +0200

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""a Python script that takes in a URL, sends a
+request to the URL and displays the body of the response"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print(f"Error code: {response.status_code}")
