e6ab4c2ec2848021dda8e8f338f90173ff639c59
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 05:53:47 +0200

    Gave it the execuation permission

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/usr/bash
-# a  script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+# a script that sends a request to a URL passed as an argument then shows the status code
 curl -s -o /dev/null -w '%{http_code}' "$1"
