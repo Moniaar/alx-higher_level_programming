f7194dcdf25d4626d2eed44477c11a5f6613f63a
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 06:20:52 +0200

    #6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" a Python script that takes in a URL and an email
+address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays
+the body of the response."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    playload = {"email": sys.argv[2]}
+    response = requests.post(sys.argv[1], data=playload)
+    print(response.text)
