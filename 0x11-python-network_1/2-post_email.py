0ec44d3633610609370110539ceb3405a194774e
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 06:10:44 +0200

    #22 fixed a pycodestyle error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a Python script that takes in a URL, sends a request to
-the URL and displays the value of the X-Request-Id variable found in the header of the response"""
+the URL and displays the value of the X-Request-Id variable
+found in the header of the response"""
 import sys
 import urllib.request
 import urllib.parse
