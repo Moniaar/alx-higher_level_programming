a9e48c45a406aabaa7e3430acd5180f3075bce62
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 06:12:53 +0200

    #3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
+"""sends a request to the URL and displays the body of the
+response (decoded in utf-8)."""
 import urllib.request
 import urllib.error
 import sys
 
-def fetch_url_body(url):
-    try:
-        # Send a request and display the body of the response
-        with urllib.request.urlopen(url) as response:
-            response_body = response.read().decode('utf-8')
-            print(response_body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
 
 if __name__ == "__main__":
-    # Check if URL parameter is provided
-    if len(sys.argv) < 2:
-        print("script.py <URL>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    fetch_url_body(url)
-
+    request = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print(f"Error code: {err.code}")
