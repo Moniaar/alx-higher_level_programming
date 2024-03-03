5fbacf24a9b45e8e4bd89bf9988e7e430fd7620a
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-03 06:23:15 +0200

    #9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to
+display your id"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    b_auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=b_auth)
+    print(r.json().get("id"))
