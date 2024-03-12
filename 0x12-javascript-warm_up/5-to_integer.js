993060aa042ca9f4809beec729f2db79320560ef
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-12 10:42:26 +0200

    #23 Fixed bad interpreter error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const args = process.argv.slice(2);
 const firstArgument = parseInt(args[0]);
 
-if (!isNaN(firstArgument)) {
+if (isNaN(firstArgument)) {
   console.log("My number: ${firstArgument}");
 } else {
   console.log("Not a number");
