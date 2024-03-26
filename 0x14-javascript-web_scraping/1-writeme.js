61f1f715d35eef37ea152bf0a99f403133adfa25
moniaar <www.omniaahmed1562001@gmail.com>
2024-03-26 15:09:38 +0200

    #6 Removed the File has been written successfully section'

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,6 @@ function writeStringToFile (filePath, content) {
   fs.writeFile(filePath, content, 'utf-8', (err) => {
     if (err) {
       console.error(err);
-    } else {
-      console.log('File has been written successfully.');
     }
   });
 }
