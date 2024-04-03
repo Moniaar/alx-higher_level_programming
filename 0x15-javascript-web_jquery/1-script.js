14ad2742c9940068d174c33e020b78a58c392cc2
moniaar <www.omniaahmed1562001@gmail.com>
2024-04-03 23:38:26 +0200

    #3 With JQuery code

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Ensures that the code inside it runs only after the HTML document is fully loaded
+$(document).ready(function () {
+// The css() function is then used to change the text color to red
+  $('header').css('color', '#FF0000');
+});
