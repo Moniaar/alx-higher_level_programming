36eca176dd1bce3ae3c3d172b114f8a539a7a0a7
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-04-04 02:39:09 +0200

    Removed var and put const instead

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var header = document.querySelector('header');
+  const header = document.querySelector('header');
   header.style.color = '#FF0000';
 });
